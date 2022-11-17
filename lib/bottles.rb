74b21d88dfcb893a372c2f959a29c2495514ca3f
Patrik Ragnarsson <patrik@starkast.net>
2022-11-17 12:08:44 +0100

    Pass: test_verse_2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,14 @@
 class Bottles
+  def bottles_str(bottles)
+    return "1 bottle" if bottles == 1
+
+    "#{bottles} bottles"
+  end
+
   def verse(bottles)
-    "#{bottles} bottles of beer on the wall, " +
-    "#{bottles} bottles of beer.\n" +
+    "#{bottles_str(bottles)} of beer on the wall, " +
+    "#{bottles_str(bottles)} of beer.\n" +
     "Take one down and pass it around, " +
-    "#{bottles - 1} bottles of beer on the wall.\n"
+    "#{bottles_str(bottles - 1)} of beer on the wall.\n"
   end
 end
