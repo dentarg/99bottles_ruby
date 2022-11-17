04826025f4608594209c95f37d5d54b2c19ca988
Patrik Ragnarsson <patrik@starkast.net>
2022-11-17 12:18:27 +0100

    Pass: test_verse_0

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,5 +1,7 @@
 class Bottles
-  def bottles_str(bottles)
+  def bottles_str(bottles, capitalize = true)
+    no_str = capitalize ? "No" : "no"
+    return "#{no_str} more bottles" if bottles == 0
     return "1 bottle" if bottles == 1
 
     "#{bottles} bottles"
@@ -19,8 +21,13 @@ class Bottles
 
   def verse(bottles)
     "#{bottles_str(bottles)} of beer on the wall, " +
-    "#{bottles_str(bottles)} of beer.\n" +
-    "Take #{down_str(bottles)} down and pass it around, " +
-    "#{after_down_str(bottles - 1)} of beer on the wall.\n"
+    "#{bottles_str(bottles, false)} of beer.\n" +
+    if bottles > 0
+      "Take #{down_str(bottles)} down and pass it around, " +
+      "#{after_down_str(bottles - 1)} of beer on the wall.\n"
+    else
+      "Go to the store and buy some more, " +
+      "99 bottles of beer on the wall.\n"
+    end
   end
 end
